d75e85849545fd2a7e34996cef5118b802018f10
ZUITOPIA <korhwngml123@naver.com>
2023-05-16 17:43:31 +0900

    Feat: main 파일 수정중

--- baseCode_Capstone/L3_FSMevent.h
+++ baseCode_Capstone/L3_FSMevent.h
@@ -1,9 +1,17 @@
 typedef enum L3_event
 {
-    L3_event_msgRcvd = 2,
-    L3_event_dataToSend = 4,
-    L3_event_dataSendCnf = 5,
-    L3_event_recfgSrcIdCnf = 6
+    // L3_event_msgRcvd = 2,
+    // L3_event_dataToSend = 4,
+    // L3_event_dataSendCnf = 5,
+    // L3_event_recfgSrcIdCnf = 6,
+
+    L3_event_CALLON_REQ_SEND = 0, // 송신
+    L3_event_CALLON_REQ_CNF = 1, // 수신
+    L3_event_CALLOFF_REQ_SEND = 2, // 송신
+    L3_event_CALLOFF_REQ_CNF = 3, // 수신
+    L3_event_TIMER_START = 4,
+    L3_event_TIMEOUT = 5,
+
 } L3_event_e;
 
 
